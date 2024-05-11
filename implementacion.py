63969fdd8a3c6dab65806f23bcf6e977c00ebffc
a-barearodriguez-um-es <a.barearodriguez@um.es>
2024-05-11 11:02:47 +0200

    implementacion practicamente terminada

--- implementacion.py
+++ implementacion.py
@@ -10,21 +10,22 @@ class SistemaIoT:
     _instance = None
 
     @classmethod
-    def obtener_instancia(cls):
+    def obtener_instancia(cls, estrategia=None):
         if cls._instance is None:
-            cls._instance = cls()
+            if estrategia is None:
+                raise Exception("No se ha seleccionado una estrategia")
+            cls._instance = cls(estrategia)
         return cls._instance
 
-    def __init__(self):
-        if SistemaIoT._instance is not None:
-            raise Exception("Esta clase es un singleton")
-        self.datos_temporales = deque(maxlen=12)  # Datos de los últimos 60 segundos (12*5)
-        self.cadena_manejo = CalculoEstadisticas(MediaDesviacionStrategy())
+    def __init__(self, estrategia):
+        self.estrategia = estrategia
+        self.datos_temporales = deque(maxlen=12)  # Datos de los últimos 60 segundos (12*5) cola de maximo 12 elementos
         self.tiempo_inicial = time.time()
+        self.manejador = CalculoEstadisticas(self.estrategia)
 
     def procesar_dato(self, dato,tiempo_inicial):
         self.datos_temporales.append(dato)
-        self.cadena_manejo.manejar_dato(list(self.datos_temporales), tiempo_inicial)
+        self.manejador.manejar_dato(list(self.datos_temporales), tiempo_inicial)
 
 # Observer
 class Sensor:
@@ -49,11 +50,10 @@ class Estrategia(ABC):
 
 class MediaDesviacionStrategy(Estrategia):
         
-    def ejecutar(self, datos, tiempo_inicial):
-        if (time.time() - tiempo_inicial) %60 < 0.1:
-            media = sum(d[1] for d in datos) / len(datos)
-            desviacion = (sum((x[1] - media) ** 2 for x in datos) / len(datos)) ** 0.5
-            print(f'Media: {media}, Desviación: {desviacion}')
+    def ejecutar(self, datos):
+        media = sum(d[1] for d in datos) / len(datos)
+        desviacion = (sum((x[1] - media) ** 2 for x in datos) / len(datos)) ** 0.5
+        print(f'Media: {media}, Desviación: {desviacion}')
 
 class CuantilesStrategy(Estrategia):
     def ejecutar(self, datos):
@@ -73,36 +73,38 @@ class Manejador:
     def __init__(self, sucesor=None):
         self.sucesor = sucesor
 
-    def manejar_dato(self, datos):
+    def manejar_dato(self, datos, tiempo_inicial):
         if self.sucesor:
-            self.sucesor.manejar_dato(datos)
+            self.sucesor.manejar_dato(datos, tiempo_inicial)
 
 class CalculoEstadisticas(Manejador):
     def __init__(self, estrategia):
         super().__init__(Umbral(25))
         self.estrategia = estrategia
-
+ 
     def manejar_dato(self, datos,tiempo_inicial):
-        self.estrategia.ejecutar(datos, tiempo_inicial)
-        super().manejar_dato(datos)
+        if (time.time() - tiempo_inicial) %60 < 0.3: # no podmos poner ==0 porque nunca seran 60 segundos exactos
+            
+            self.estrategia.ejecutar(datos)
+        super().manejar_dato(datos, tiempo_inicial)
 
 class Umbral(Manejador):
     def __init__(self, umbral):
         super().__init__(Aumento(10))
         self.umbral = umbral
 
-    def manejar_dato(self, datos):
+    def manejar_dato(self, datos, tiempo_inicial):
         ultimo_dato = datos[-1][1] if datos else None
         if ultimo_dato and ultimo_dato > self.umbral:
             print(f"-La temperatura ha superado el umbral de {self.umbral} Cº.")
-        super().manejar_dato(datos)
+        super().manejar_dato(datos, tiempo_inicial)
 
 class Aumento(Manejador):
     def __init__(self, incremento_maximo):
         super().__init__()
         self.incremento_maximo = incremento_maximo
 
-    def manejar_dato(self, datos):
+    def manejar_dato(self, datos, tiempo_inicial):
         cortar = True   
         if len(datos) >= 6 and cortar == True:
             for i in range(len(datos)-5,len(datos)-1):
@@ -112,24 +114,32 @@ class Aumento(Manejador):
                     if temperatura_final - temperatura_inicial > self.incremento_maximo:
                         print("-Aumento de +10Cº en los ultimos 30 segundos")
                         cortar = False
-        super().manejar_dato(datos)
+        super().manejar_dato(datos, tiempo_inicial)
 import time
 
 # Definir las clases y funciones aquí...
 
 def main():
-    sistema = SistemaIoT.obtener_instancia()
+    n = int(input("que estrategia quieres usar:"))
+    if n ==1:
+        estrategia = MediaDesviacionStrategy()
+    elif n==2:
+        estrategia = CuantilesStrategy()
+    else:
+        estrategia = MinMaxStrategy()
+    sistema = SistemaIoT.obtener_instancia(estrategia)
     sensor = Sensor()
 
     # Registramos el sistema IoT como observador del sensor
     sensor.registrar_observador(sistema)
 
     # Simulamos la recepción de datos de temperatura cada 5 segundos durante 2 minutos
-    tiempo_0 = time.time()
     ultimo_dato = 20  # Temperatura inicial
     temperaturas = []
+    tiempo_ejecucion = int(input("cuanto tiempo quieres que dure la simulacion: "))
+    tiempo_0 = time.time()
 
-    while time.time() - tiempo_0 < 120:  # Simular durante 2 minutos
+    while time.time() - tiempo_0 < tiempo_ejecucion:  # Simular durante 2 minutos
         time.sleep(5) #para que empiece 
         # Generamos un cambio de temperatura entre -2 y +6 grados
         cambio = random.uniform(-2, 6)
@@ -138,7 +148,7 @@ def main():
        
         temperaturas.append(nuevo_dato)
         sensor.recibir_dato(dato, tiempo_0)
-        print(f"-La temparutra es: {round(nuevo_dato,2)} grados.")
+        print(f"-La temparutra es: {round(nuevo_dato,2)} grados. Han pasado {int(time.time()-tiempo_0)} segundos.")
         print("---------------------------------------------------------------")
 
         ultimo_dato = nuevo_dato
