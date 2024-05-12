e3519cb7e5ba67cd0ae680bbc58735344f5dc266
a-barearodriguez-um-es <a.barearodriguez@um.es>
2024-05-12 19:36:57 +0200

    test

--- test_impletentacion.py
+++ test_impletentacion.py
@@ -18,18 +18,18 @@ def sistema_iot():
 def sensor():
     return Sensor()
 
-def test_singleton_instance(sistema_iot):
-    # Verificar que solo haya una instancia de SistemaIoT
+def test_singleton_instance(sistema_iot):  # Verifica que solo haya una instancia de SistemaIoT
+   
     otra_instancia = SistemaIoT.obtener_instancia()
     assert sistema_iot is otra_instancia
 
-def test_observer_register(sensor, sistema_iot):
-    # Verificar que el sistema IoT se registra correctamente como observador del sensor
+def test_observer_register(sensor, sistema_iot):  # Verifica que el sistema IoT se registra correctamente como observador del sensor
+   
     sensor.registrar_observador(sistema_iot)
     assert sistema_iot in sensor._observadores
 
-def test_media_desviacion_strategy():
-    # Prueba para la estrategia de cálculo de media y desviación estándar
+def test_media_desviacion_strategy():  # Prueba para la estrategia de cálculo de media y desviación estándar
+    
     strategy = MediaDesviacionStrategy()
     datos = [(1, 20), (2, 25), (3, 30)]
     with patch('builtins.print') as mock_print:
@@ -37,24 +37,21 @@ def test_media_desviacion_strategy():
         
         mock_print.assert_called_with('Media: 25.0, Desviación: 4.08')
 
-def test_cuantiles_strategy():
-    # Prueba para la estrategia de cálculo de cuantiles
+def test_cuantiles_strategy():   # Prueba para la estrategia de cálculo de cuantiles
+   
     strategy = CuantilesStrategy()
     datos = [(0,0),(1, 10), (2, 20), (3, 30), (4, 40), (5, 50), (6, 60), (7, 70), (8, 80), (9, 90), (10, 100)]
     with patch('builtins.print') as mock_print:
         strategy.ejecutar(datos)
         mock_print.assert_called_with('Cuantil 25%: 25.0, Cuantil 75%: 75.0')
 
-def test_min_max_strategy():
-    # Prueba para la estrategia de cálculo de mínimo y máximo
+def test_min_max_strategy():  # Prueba para la estrategia de cálculo de mínimo y máximo
+    
     strategy = MinMaxStrategy()
     datos = [(1, 20), (2, 25), (3, 30), (4, 35)]
     with patch('builtins.print') as mock_print:
         strategy.ejecutar(datos)
         mock_print.assert_called_with('Mínimo: 20, Máximo: 35')
-
-
-
 
 
 def test_aumento_datos_vacios():
